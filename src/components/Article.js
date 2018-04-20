9f4733e5316fd3c4389da572ecb3f4089e835ade
olga <odnews10@gmail.com>
2018-04-20 18:00:09 +0300

    react_basic

---
+++ src/components/Article.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+class Article extends Component {
+	state = {
+		isOpen: false
+	}
+
+	render() {
+		const {article} = this.props;
+		const body = this.state.isOpen && <section className="card-text">{article.text}</section>;
+		const style = {width: '50%'};
+		return (
+			<div className="card mx-auto" style={style}>
+				<div className="card-header">
+					<h2>
+						{article.title}
+						<button onClick={this.handleClick} className="btn btn-primary btn-lg float-right">
+							{this.state.isOpen ? 'close' : 'open'}
+						</button>
+					</h2>
+				</div>
+				<div className="card-body">
+					<h3 className="card-subtitle text-muted">
+						creation date: {(new Date(article.date)).toDateString()}
+					</h3>
+					{body}
+				</div>
+			</div>
+		)
+	}
+
+	handleClick = () => {
+		console.log('------', 'clicked');
+		this.setState({
+			isOpen: !this.state.isOpen
+		})
+	}
+}
+
+export default Article;
---
+++ src/components/ArticleList/index.js
@@ -0,0 +1,14 @@
+import React, {PureComponent} from 'react'
+import Article from '../Article'
+import './style.css'
+
+export default function ArticleList({ articles }) {
+	const articleElements = articles.map(article => 
+		<li key = {article.id} className="article-list__li"><Article article = {article} /></li>
+	)
+	return (
+		<ul>
+			{articleElements}
+		</ul>
+	)
+}
